4d0ab5692d1ff0fec964b898661aef32ef0a71c0
tjonty <tejanijonty2323@gmail.com>
2021-04-23 15:12:52 +0530

    keeper - add mapping to component

--- google-keeper/src/components/App.jsx
+++ google-keeper/src/components/App.jsx
@@ -5,11 +5,21 @@ import Header from './Header';
 import Note from "./Note";
 import Footer from "./Footer";
 
+import notes from '../notes';
+
 const App = () => {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((note) => {
+                return (
+                    <Note 
+                        key={note.key}
+                        title={note.title}
+                        content={note.content}
+                    />
+                )
+            })}
             <Footer />
         </div>
     );
